6b7df1b1c73583e03aa27fc1c81ff0c7293e410e
kumkum-bharti <bhartikumkum57@gmail.com>
2025-12-31 21:42:10 +0530

    Product controller initiation

--- dream_shop/src/main/java/com/example/dreamshop/controller/CategoryController.java
+++ dream_shop/src/main/java/com/example/dreamshop/controller/CategoryController.java
@@ -1,8 +1,10 @@
 package com.example.dreamshop.controller;
 
+import com.example.dreamshop.exceptions.ResourceNotFoundException;
 import com.example.dreamshop.model.Category;
 import com.example.dreamshop.response.ApiResponse;
 import com.example.dreamshop.services.category.CategoryService;
+import com.example.dreamshop.services.category.ICategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.CONFLICT;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,7 +20,7 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 
 public class CategoryController {
-    private final CategoryService categoryService;
+    private final ICategoryService categoryService;
 
     @GetMapping("/all")
     public ResponseEntity<ApiResponse> getAllCategories(){
@@ -39,6 +40,47 @@ public class CategoryController {
             return ResponseEntity.ok(new ApiResponse("Added", theCategory));
         } catch (Exception e) {
             return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/category/{imageId}/category")
+    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable("imageId") Long id){
+        try {
+            Category theCategory = categoryService.getCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Found", theCategory));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @GetMapping("/category/{name}/category")
+    public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name){
+        try {
+            Category theCategory = categoryService.getCategoryByName(name);
+            return ResponseEntity.ok(new ApiResponse("Found", theCategory));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null ));
+        }
+    }
+
+    @DeleteMapping("/category/{imageId}/delete")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable("imageId") Long id){
+        try {
+            categoryService.deleteCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Deletion Successful", null));
+        } catch (Exception e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+
+    @PutMapping("/category/{id}/update")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        try {
+            Category updatedCategory = categoryService.updateCategory(category,id);
+            return ResponseEntity.ok(new ApiResponse("Update Success", updatedCategory));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null ));
         }
     }
 }
--- dream_shop/src/main/java/com/example/dreamshop/controller/ProductCategory.java
+++ dream_shop/src/main/java/com/example/dreamshop/controller/ProductCategory.java
@@ -1,4 +1,60 @@
 package com.example.dreamshop.controller;
 
+import com.example.dreamshop.exceptions.ResourceNotFoundException;
+import com.example.dreamshop.model.Product;
+import com.example.dreamshop.requests.AddProductRequest;
+import com.example.dreamshop.response.ApiResponse;
+import com.example.dreamshop.services.category.CategoryService;
+import com.example.dreamshop.services.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("{api.prefix}/products")
+
 public class ProductCategory {
+    private final IProductService productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllProducts(){
+        try {
+            List<Product> products=productService.getAllProducts();
+            return ResponseEntity.ok(new ApiResponse("success",products));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+
+    @GetMapping("/product/{id}/product")
+    public ResponseEntity<ApiResponse> getProductById(@PathVariable Long id){
+        try {
+            Product theProduct=productService.getProductById(id);
+            return ResponseEntity.ok(new ApiResponse("success",theProduct));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addProduct(@RequestBody AddProductRequest product){
+        try {
+            Product theProduct=productService.addProduct(product);
+            return ResponseEntity.ok(new  ApiResponse("Add product success",theProduct));
+        } catch (Exception e) {
+             return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+
+    
+
+
+
 }
